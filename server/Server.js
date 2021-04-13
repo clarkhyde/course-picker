287a138c18f7fc143ade9633a684d8780d431894
Clark <clarkhyde123@gmail.com>
2021-04-13 16:46:19 -0500

    fixed hashing

--- server/Server.js
+++ server/Server.js
@@ -6,12 +6,9 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
-<<<<<<< HEAD
 const usersRoute =require('./routes/users');
 
 app.use("/", usersRoute);
-=======
->>>>>>> e878d353de5ca0f3797a15b3c98eee1d235020a2
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on ${PORT}`);
