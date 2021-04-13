87940231ceecd72dadca61fb836ff578d2e5983f
Clark <clarkhyde123@gmail.com>
2021-04-13 10:48:34 -0500

    initial component folders and files created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world!
+      <p>Welcome to the course picker extraordinaire!</p>
+      <p>THis app will keep track of courses we have played. (maybe remove the course from the previous week?)</p>
     </div>
   );
 }
