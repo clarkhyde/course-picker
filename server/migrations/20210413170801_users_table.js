e330a0ed7a31101f0e228cad1928134cd2a4cdbf
Clark <clarkhyde123@gmail.com>
2021-04-13 12:47:58 -0500

    database is set up

---
+++ server/migrations/20210413170801_users_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users',(table)=>{
+      table.increments("id").primary();
+      table.string("username").unique().notNullable();
+      table.string("password").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex) {
+  
+};
